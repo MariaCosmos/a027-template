bee94c001be409e9d8bca9b41e797bcb6a63e6ac
MariaCosmos <cosmos.maria2003@gmail.com>
2022-10-03 14:10:43 -0300

    Exercícios extra - DOM I

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+let fraseSapo = document.getElementById("sapo")
+fraseSapo.innerHTML += " pé, não lava por que não quer"
+
+let frasePoema = document.getElementById("poema")
+frasePoema.innerHTML += " evidencias"
+
+let fraseSabedoria = document.getElementById("sabedoria")
+fraseSabedoria.innerHTML += " voando"
+
+let fraseBorboleta = document.getElementById("borboleta")
+fraseBorboleta.innerHTML += " pica-pau"
+
+let fraseRoberto = document.getElementById("roberto")
+fraseRoberto.innerHTML += " senti"
---
+++ exercicio3/script.js
@@ -0,0 +1,6 @@
+let num = 0
+
+function contarClique(){
+  num += 1
+  console.log(num)
+}
